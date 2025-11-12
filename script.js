e3e47a16b5ddab8360c84a565b4466cd8ed11359
Qasim <qasimalhardanee@gmail.com>
2025-11-12 14:34:24 +0300

    Create openFolder function for empty folder alert
    Added a function to alert when a folder is empty.

---
+++ script.js
@@ -0,0 +1,5 @@
+function openFolder(folderName) {
+  alert(folderName + " klasörü şu anda boş. İleride içeriği eklenecek!");
+  // İleride buraya yönlendirme veya alt dizin açma kodu ekleyebilirsin.
+  // Örneğin: window.location.href = folderName + "/index.html";
+}
